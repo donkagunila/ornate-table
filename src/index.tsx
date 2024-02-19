91d1007b974f040a8cd1dbf338a0b3bb54cf82d3
Donald Kagunila <donald.kagunila@crdbbank.co.tz>
2024-02-19 18:47:40 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "styles/globals.css";
 
 export const OrnateTable = () => {
-  return <div>OrnateTable</div>;
+  return (
+    <div className="card">
+      <div className="card-header flex">
+        <div className="flex justify-content-center items-center px-3 w-full">
+          <div>
+            <h2 className="text-base font-medium mx-3 text-slate-600 ">
+              Ornate Table will come here
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
